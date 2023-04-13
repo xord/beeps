1f2b4b1e65ebd64473ee2f9c25485f25582105e6
xordog <xordog@gmail.com>
2023-04-14 00:25:33 +0900

    add test_file_in.rb

---
+++ test/test_file_in.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+require_relative 'helper'
+
+
+class TestFileIn < Test::Unit::TestCase
+
+  B = Beeps
+
+  def test_initialize()
+    assert_raise(B::BeepsError) {B::FileIn.new 'nofile.wav'}
+  end
+
+end# TestFileIn
