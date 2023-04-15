6280f68285cfb694f642f53a120f5b1a29946a79
xordog <xordog@gmail.com>
2023-04-15 10:10:09 +0900

    Sound.new can take block

--- lib/beeps/sound.rb
+++ lib/beeps/sound.rb
@@ -12,8 +12,9 @@ module Beeps
 
   class Sound
 
-    def initialize(processor, seconds = 0, nchannels: 1, sample_rate: 0)
+    def initialize(processor, seconds = 0, nchannels: 1, sample_rate: 0, &block)
       setup processor, seconds, nchannels, sample_rate
+      Xot::BlockUtil.instance_evan_or_block_call self, &block if block
     end
 
     def play(options = nil, &block)
