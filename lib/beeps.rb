f9f3b52968b24514a372a6406df6aa9f7bf212db
xordog <xordog@gmail.com>
2024-05-04 01:17:36 +0900

    test

--- lib/beeps.rb
+++ lib/beeps.rb
@@ -1,4 +1,6 @@
+p [20, Time.now.to_f]
 require 'beeps/autoinit'
+p [21, Time.now.to_f]
 #require 'beeps/extension'
 
 #require 'beeps/beeps'
--- lib/beeps/autoinit.rb
+++ lib/beeps/autoinit.rb
@@ -1,7 +1,12 @@
 require 'beeps/ext'
 
 
+p [38, Time.now.to_f]
 unless defined?($BEEPS_NOAUTOINIT) && $BEEPS_NOAUTOINIT
+  p [30, Time.now.to_f]
   Beeps.init!
+  p [31, Time.now.to_f]
   at_exit {Beeps.fin!}
+  p [32, Time.now.to_f]
 end
+p [39, Time.now.to_f]
