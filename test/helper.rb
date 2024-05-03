8a73f6cf64a158b8ba31a7974f8ab48644ffcc0a
xordog <xordog@gmail.com>
2024-05-03 23:16:28 +0900

    test

--- test/helper.rb
+++ test/helper.rb
@@ -2,14 +2,14 @@
   .map  {|s| File.expand_path "../#{s}/lib", __dir__}
   .each {|s| $:.unshift s if !$:.include?(s) && File.directory?(s)}
 
-require 'xot/test'
-require 'beeps'
+#require 'xot/test'
+#require 'beeps'
 
 require 'test/unit'
 
-include Xot::Test
+#include Xot::Test
 
 
 unless defined?($BEEPS_NOAUTOINIT) && $BEEPS_NOAUTOINIT
-  def Beeps.fin!() end
+  #def Beeps.fin!() end
 end
